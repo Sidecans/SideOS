55e63d09f7492f68a8383226a4d3d8eb6d5ac883
Sidecans <110431851+Sidecans@users.noreply.github.com>
2022-08-11 16:30:55 -0700

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,7 +1,11 @@
 import pickle
 class db():
-  def __init__(self, obj, name):
-    self.file = name + ".sideDB"
+  def __init__(self, name):
+    self.file = str(name) + ".sideDB"
+  def dump(self, obj)
     with open(self.file, 'wb') as f:
       pickle.dump(obj, f)
-    
+  def load(self):
+    with open(self.file, 'rb') as f:
+      pickle.load(f)
+      

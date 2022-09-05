062ef5d13d0eac33c4f8cd5f00ec25537bf3f88b
Sidecans <sidbhave@hotmail.com>
2022-09-05 16:51:04 -0700

    Update os.py

--- os.py
+++ os.py
@@ -1,6 +1,41 @@
 import xml.etree.ElementTree as XML
 import sys
+import threading
+import time
+import os
+
+
+GRAY = "\033[0;90m"
+PINK = "\033[0;91m"
+LIME = "\033[0;92m"
+YELLOW = "\033[0;93m"
+AQUA = "\033[0;94m"
+MAGENTA = "\033[0;95m"
+TEEL = "\033[0;96m"
+
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+WHITE = "\033[0;37m"
+
+
+def setup():
+    print(BLUE + "Hello")
+    time.sleep(1)
+    os.system('cls')
+
+
+ 
 
 tree = XML.parse("OS\\Drive\\ProgramFiles\\Sidecans\\OS\\Data\\UserData.xml")
 root = tree.getroot()
-print(root[0].text)
+
+if root[0].text == "0":
+    
+    t1 = threading.Thread(target=setup, name="t1")
+    t1.start()
+    
